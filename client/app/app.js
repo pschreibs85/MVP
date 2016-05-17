3a6f3758ea8928335beb7998004167f040c0a50e
Paul-Michael Schreiber <paulmichael.schreiber@gmail.com>
2016-05-16 20:59:43 -0500

    attemped to get search autofill to work

---
+++ client/app/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// angular.module('myApp', [
+//     'ui.router'
+//   ])
+
+//     .config(function($stateProvider, $urlRouterProvider) {
+    
+//     $urlRouterProvider.otherwise('/');
+    
+//     $stateProvider
+//         .state('home', {
+//             url: '/',
+//             templateUrl: 'views/main.html',
+//             controller: 'MainCtrl'
+//         })
+
+//         .state('about', {
+//           url: '/about',
+//           templateUrl: 'views/about.html',
+//           controller: 'AboutCtrl'
+//         });
+        
+// });
+
+
+angular.module('ppdb', [
+  'ppdb.topRated',
+  'ppdb.search',
+  'ppdb.about',
+  'ppdb.contact',
+  'ngRoute'
+])
+.config(function($routeProvider, $httpProvider) {
+
+  $routeProvider
+    .when('/toprated', {
+      templateUrl: 'views/topRated.html',
+      controller: 'mainController'
+    })
+    .when('/search', {
+      templateUrl: 'views/search.html',
+      controller: 'mainController'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'mainController'
+    })
+    .when('/contact', {
+      templateUrl: 'views/contact.html',
+      controller: 'mainController'
+    })
+   .otherwise('/');
+
+})
+
+    
---
+++ client/app/services/services.js
@@ -0,0 +1,35 @@
+
+// angular.module('ppdb.services', [])
+
+// .factory('Generate', function ($http) {
+
+
+//   var getLinks = function () {
+//     return $http({
+//       method: 'GET',
+//       url: '/api/links',
+//     })
+//     .then(function (resp) {
+//       return resp.data;
+//     });
+//   }
+
+//   var addLink = function (link) {
+//     console.log(link)
+//     return $http({
+//       method: 'POST',
+//       url: '/api/links',
+//       data: link
+//     })
+//     .then(function (resp) {
+//       console.log(resp)
+//       return resp.data;
+//     });
+//   }
+
+//   return {
+//     getLinks: getLinks,
+//     addLink: addLink
+//   }
+
+// })
